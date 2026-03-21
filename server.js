b2fbd9c02473ba36dd8455c474b9d75246c5afdb
Steven Kay <steven.m.kay@live.com>
2026-03-21 16:46:22 -0700

    Add support for URL-encoded request bodies

--- server.js
+++ server.js
@@ -59,6 +59,7 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const API_KEY = process.env.YOUTUBE_API_KEY || 'YOUR_YOUTUBE_API_KEY';
 
